a440490951f1871eb4c142ced6a0dd17afa024fb
Kai <kladouceur06@gmail.com>
2021-07-04 21:31:03 -0400

    asdw

--- jury-game.js
+++ jury-game.js
@@ -1,12 +1,21 @@
 let post = {
     "pos": 0,
-    0: ["Some text", 0],
-    1: ["Another text", 0],
-    2: ["Final text", 0]
+    0: ["text", 0],
+    1: ["text", 0],
+    2: ["text", 0]
 };
 
+let score = 0; 
+let count = 0;
 function score_post(post, mod){
-    post[post["pos"]][1] += mod;
+    post['count']++
+    if post[post["pos"]][1]{
+        post["score"]++;
+    }
+    // post[post["pos"]][1] += mod;
+    if (count == post['tot']){
+        post['']
+    }
     new_post(post);
 }
 
@@ -14,8 +23,13 @@ function new_post(post){
     // This is until we get a real way to implement this
     if (post["pos"] < 2)
         post["pos"]++;
-    else
-        post["pos"] = 0;
+    else 
+        game_over();
 
     document.getElementById("post_container").innerHTML = post[post["pos"]][0];
+}
+
+function game_over(){
+
+
 }
